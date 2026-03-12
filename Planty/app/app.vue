17dee0b2ac17e042a00de67488666a2c158a8040
cag185 <cag185@pitt.edu>
2026-03-12 17:13:25 -0400

    initial commit

---
+++ Planty/app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center w-full bg-lime-100 h-screen">
+    <div class="w-1/2">
+      <div class="text-4xl text-center">Welcome to Planty</div>
+      <div class="text-xl text-center">The plant automous tracker</div>
+    </div>
+  </div>
+</template>
